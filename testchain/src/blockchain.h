45bc266d02d78e1694fc3b22ef0e65085351da69
Inhyeok Bang <bih3754@gmail.com>
2022-01-10 20:26:21 +0900

    added sha256 source from zedwood

---
+++ testchain/src/blockchain.h
@@ -0,0 +1,32 @@
+/**
+ * Copyright 2022 Inhyeok Bang
+ *
+ * @brief     Blockchain tutorial project 'TestChain' with C++
+ * @file      blockchain.h
+ * @reference https://davenash.com/2017/10/build-a-blockchain-with-c/
+ */
+
+#ifndef TESTCHAIN_SRC_BLOCKCHAIN_H_
+#define TESTCHAIN_SRC_BLOCKCHAIN_H_
+
+#include <cstdint>
+#include <vector>
+
+#include "block.h"
+
+using std::vector;
+
+class Blockchain {
+ public:
+  Blockchain();
+
+  void AddBlock(Block new_block);
+
+ private:
+  uint32_t difficulty_;
+  vector<Block> chain_;
+
+  Block GetLastBlock() const;
+};
+
+#endif  // TESTCHAIN_SRC_BLOCKCHAIN_H_
